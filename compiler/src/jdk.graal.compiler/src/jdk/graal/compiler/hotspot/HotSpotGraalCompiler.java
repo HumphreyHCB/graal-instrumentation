650845aaf7918b9046646a8b4aaca98d01f19797
Humphrey Burchell <humphreyburchell@gmail.com>
2023-09-12 17:32:03 +0100

    added My Print

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotGraalCompiler.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotGraalCompiler.java
@@ -107,6 +107,7 @@ public class HotSpotGraalCompiler implements GraalJVMCICompiler, Cancellable, JV
 
     @Override
     public CompilationRequestResult compileMethod(CompilationRequest request) {
+        System.out.println("Humphrey compiler compiling " + request.getMethod());
         return compileMethod(this, request);
     }
 
