d78a7312096a492333292702f47d1bc83f1a8aef
Humphrey Burchell <humphreyburchell@gmail.com>
2023-09-25 10:25:11 +0100

    added some broken rubbish :)

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/nodes/CustomInstrumentationNode.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/nodes/CustomInstrumentationNode.java
@@ -27,12 +27,22 @@ package org.graalvm.compiler.nodes;
 import static org.graalvm.compiler.nodeinfo.NodeCycles.CYCLES_2;
 import static org.graalvm.compiler.nodeinfo.NodeSize.SIZE_1;
 
+import org.graalvm.compiler.core.common.LIRKind;
+import org.graalvm.compiler.core.common.type.Stamp;
 import org.graalvm.compiler.core.common.type.StampFactory;
 import org.graalvm.compiler.graph.NodeClass;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.spi.LIRLowerable;
 import org.graalvm.compiler.nodes.spi.Lowerable;
 import org.graalvm.compiler.nodes.spi.NodeLIRBuilderTool;
+import org.graalvm.compiler.replacements.SnippetCounter;
+import org.graalvm.compiler.replacements.SnippetCounterNode;
+import org.graalvm.compiler.replacements.SnippetCounter.Group;
+
+import jdk.vm.ci.code.Register;
+import jdk.vm.ci.meta.JavaConstant;
+import jdk.vm.ci.meta.Value;
+import jdk.vm.ci.meta.*;
 
 /**
  * Marks a position in the graph where a node should be emitted.
@@ -52,8 +62,31 @@ public final class CustomInstrumentationNode extends DeoptimizingFixedWithNextNo
 
     @Override
     public void generate(NodeLIRBuilderTool gen) {
+
         //gen.state(this);
+        ConstantNode constNextInt = new ConstantNode(JavaConstant.forInt(1), StampFactory.intValue());
+        constNextInt.asNode();
+        SnippetCounter sc = new SnippetCounter(new Group("My counter group"), "my counter", "HI this is my counter");
+        SnippetCounterNode scn = new SnippetCounterNode(sc, constNextInt);
+        //sc.add(1);
+        //scn.increment(sc);
+
+        Register register = new Register(NOT_ITERABLE, NOT_ITERABLE, null, null);
+        LIRKind kind = gen.getLIRGeneratorTool().getLIRKind(scn.getIncrement().stamp);
+        Value result = register.asValue(kind);
+
+        gen.setResult(constNextInt.asNode(), result);
     }
+
+    // LIRKind kind = generator.getLIRGeneratorTool().getLIRKind(stamp());
+    // Value result = register.asValue(kind);
+    // if (incoming) {
+    //     generator.getLIRGeneratorTool().emitIncomingValues(new Value[] { result });
+    // }
+    // if (!directUse) {
+    //     result = generator.getLIRGeneratorTool().emitMove(result);
+    // }
+    // generator.setResult(this, result);
 
     @Override
     public boolean canDeoptimize() {
