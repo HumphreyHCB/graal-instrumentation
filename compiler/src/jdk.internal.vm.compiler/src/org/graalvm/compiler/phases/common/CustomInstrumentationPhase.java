78972da779553fe4ae37f050ca14b40a97da8cbb
Humphrey Burchell <humphreyburchell@gmail.com>
2023-11-08 15:06:43 +0000

    capture invokes

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/phases/common/CustomInstrumentationPhase.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/phases/common/CustomInstrumentationPhase.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 
 import org.graalvm.compiler.replacements.SnippetCounter.Group;
 import org.graalvm.compiler.core.CompilationWrapper.ExceptionAction;
@@ -43,6 +44,7 @@ import org.graalvm.compiler.nodes.IfNode;
 import org.graalvm.compiler.nodes.Invoke;
 import org.graalvm.compiler.nodes.InvokeNode;
 import org.graalvm.compiler.nodes.GraphState.StageFlag;
+import org.graalvm.compiler.nodes.extended.OSRStartNode;
 import org.graalvm.compiler.nodes.java.MethodCallTargetNode;
 import org.graalvm.compiler.nodes.LoopBeginNode;
 import org.graalvm.compiler.nodes.LoopEndNode;
@@ -129,24 +131,61 @@ public class CustomInstrumentationPhase extends BasePhase<HighTierContext>  {
     @SuppressWarnings("try")
     protected void run(StructuredGraph graph, HighTierContext context) {
 
-            for (ResolvedJavaMethod method : graph.getMethods()) {
-                System.out.println("ResolvedJavaMethod  "+method.getName());
-                
-            }
-  
-            for (ResolvedMethodHandleCallTargetNode methodcall : graph.getNodes(ResolvedMethodHandleCallTargetNode.TYPE)) {
-                System.out.println("ResolvedMethodHandleCallTargetNode  "+methodcall.targetName());
-                                System.out.println("ResolvedMethodHandleCallTargetNode  toString"+methodcall.toString());
-            }
+            // for (ResolvedJavaMethod method : graph.getMethods()) {               
+            //     System.out.println("ResolvedJavaMethod  "+method.getName());
+            // }
+
+
+            // for (Node node : graph.getNodes()) {
+            //     System.out.println("Node  " + node.toString());
+            //     //System.out.println("Node  asJavaConstant " + methodcall.asNode().asJavaConstant().toString());
+            //     //System.out.println("Node  getNodeSourcePosition " + methodcall.getNodeSourcePosition().toString());
+            //     //System.out.println("Node  DebugProperties" + node.getDebugProperties());
+
+            // }
+
+
+            // for (StartNode methodcall : graph.getNodes(StartNode.TYPE)) {
+            //     System.out.println("Node  " + methodcall.getDebugProperties());
+            //     System.out.println("Node  getCurrentScopeName " + methodcall.getDebug().getCurrentScopeName());
+            //     System.out.println("Node  toString "+methodcall.toString());
+            // }
 
 
             for (Invoke invokes : graph.getInvokes()) {
-                System.out.println(invokes.callTarget().targetName());
+                //System.out.println(invokes.callTarget().targetName());
                 try (DebugCloseable s = invokes.asFixedNode().withNodeSourcePosition()) {
                 CustomInstrumentationNode CustomInstrumentationNode = graph.add(new CustomInstrumentationNode(invokes.callTarget().targetName(),group));
                 graph.addBeforeFixed(invokes.asFixedNode(), CustomInstrumentationNode);  
                 }             
             }
+
+            // for (LoopBeginNode loopBeginNode : graph.getNodes(LoopBeginNode.TYPE)) {
+
+            //     for (FixedNode node :  loopBeginNode.getBlockNodes()) {
+            //         //System.out.println("First loop: " + node.toString());
+            //         // find all if nodes follwoing the loop begiun
+            //         if (node.getClass().equals(IfNode.class)) {
+            //              //System.out.println("in loop loop: " + node.toString());
+            //              IfNode ifnode = ((IfNode)node);
+            //              // find all of the begin nodes that follow the if
+            //              for (Node sucnode  : ifnode.cfgSuccessors()) {
+            //                 CustomInstrumentationNode CustomInstrumentationNode = graph.add(new CustomInstrumentationNode(sucnode.toString() + sucnode.getDebug().toString(),group));
+            //                 graph.addAfterFixed((FixedWithNextNode) sucnode, CustomInstrumentationNode);
+            //              }
+            //         }
+            //     }
+               
+            //     for (LoopEndNode loopEndNode : loopBeginNode.loopEnds()) {
+                        
+            //             try (DebugCloseable s = loopEndNode.withNodeSourcePosition()) {
+            //                 CustomInstrumentationNode CustomInstrumentationNode = graph.add(new CustomInstrumentationNode(loopEndNode.toString(),group));
+            //                 graph.addBeforeFixed(loopEndNode, CustomInstrumentationNode);
+
+            //             }
+                    
+            //     }
+            // }
             
         
     }
