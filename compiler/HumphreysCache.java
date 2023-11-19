f68c7d5643a33d6a8745a04cbf6990025066efca
Humphrey Burchell <humphreyburchell@gmail.com>
2023-11-19 22:51:41 +0000

    Cache example

---
+++ compiler/HumphreysCache.java
@@ -0,0 +1,29 @@
+import java.lang.Thread;
+
+public class HumphreysCache extends Thread {
+        
+        public static long[] Buffer;
+        public static int pointer;
+
+        public HumphreysCache() {
+                Buffer = new long[3_000_000];
+                pointer = 0;
+
+        }
+
+        public void add(long item)
+        {
+                Buffer[pointer] = item;
+                pointer++;
+
+        }
+
+        public void print(){
+
+                for (long l : Buffer) {
+                        System.out.print(l);
+                }
+        }
+
+
+}
