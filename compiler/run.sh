ffb98d414a522917a6f8e39f65649b582d677a09
Humphrey Burchell <humphreyburchell@gmail.com>
2024-01-24 22:30:13 +0000

    extra command that could be used

--- compiler/run.sh
+++ compiler/run.sh
@@ -14,9 +14,6 @@ mx --java-home \
   -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar \
   HelloWorld 
 
-#   mx --java-home \
-# /home/hburchell/Downloads/labsjdk-ce-17.0.9+4-jvmci-23.0-b17-linux-amd64/labsjdk-ce-17.0.9-jvmci-23.0-b17 \
-#  vm \
-#  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -Dgraal.Dump -XX:+UseJVMCICompiler -Dgraal.SnippetCounters=true \
-#   -Dgraal.Counters= -XX:CompileOnly=HelloWorld -XX:-TieredCompilation \
-#   HelloWorld 
+# mx --java-home /home/hburchell/Downloads/labsjdk-ce-17.0.9+4-jvmci-23.0-b17-linux-amd64/labsjdk-ce-17.0.9-jvmci-23.0-b17  vm  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI   -Dgraal.Dump="*" -Dgraal.DumpOnError=true -Dgraal.DumpingErrorsAreFatal=true   -Dgraal.CompilationFailureAction="Print"     -XX:+UseJVMCICompiler  --add-exports   jdk.internal.vm.compiler/org.graalvm.compiler.hotspot.meta.Bubo=ALL-UNNAMED   -cp /home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/mxbuild/dists/graal.jar:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler:/home/hburchell/Repos/AWFY-Profilers/AWFY/benchmarks/Java/benchmarks.jar   -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar Harness DeltaBlue 2000 100
+
+
