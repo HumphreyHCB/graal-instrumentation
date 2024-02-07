e2b09789917c55e8cb049586f19a946bb80d776b
Humphrey Burchell <humphreyburchell@gmail.com>
2024-02-07 22:52:27 +0000

    Set larger cache

--- compiler/run.sh
+++ compiler/run.sh
@@ -3,10 +3,10 @@
 
 mx --java-home \
 /home/hburchell/Downloads/labsjdk-ce-17.0.9+4-jvmci-23.0-b17-linux-amd64/labsjdk-ce-17.0.9-jvmci-23.0-b17 \
- vm \
+ vm -Xmx20g  \
  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -Dgraal.EnableProfiler=true \
  -Dgraal.Dump="*" -Dgraal.DumpOnError=true -Dgraal.DumpingErrorsAreFatal=true \
-  -Dgraal.CompilationFailureAction="Print"   \
+  -Dgraal.CompilationFailureAction="Print"  -Dlibgraal.Xmx=16G \
   -XX:+UseJVMCICompiler  \
   -XX:CompileOnly=HelloWorld -XX:-TieredCompilation --add-exports \
   jdk.internal.vm.compiler/org.graalvm.compiler.hotspot.meta.Bubo=ALL-UNNAMED \
