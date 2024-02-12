614729ccd6f980782186601bccbcceaa9fd34aa6
Humphrey Burchell <humphreyburchell@gmail.com>
2024-02-12 16:12:02 +0000

    reduced buffer

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/hotspot/meta/Bubo/BuboCache.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/hotspot/meta/Bubo/BuboCache.java
@@ -10,61 +10,11 @@ import java.lang.Thread;
  public class BuboCache extends Thread {
         
         public static long[] Buffer;
-        public static int pointer;
-        public static int BufferPointer;
-        public static long[][] BufferArray;
 
         public BuboCache() {
-                BufferArray = new long[5][250_000_000];
-                BufferPointer = 0;
-                pointer = 0;
-                Buffer = BufferArray[BufferPointer];
+                Buffer = new long[200_000];;
         }
 
-        public static void rotateBuffer() {
-                BufferPointer++;
-                if (BufferPointer >= 5) {
-                        throw new ArrayIndexOutOfBoundsException("We have reached the max of the buffer");
-                }
-                Buffer = BufferArray[BufferPointer];
-                pointer = 0;
-        }
-
-        public static void dummyPrint(long useless){
-                System.out.println("In Humphrey Cache, Dumy print");
-        }
-
-        public static void testPrint(){
-                System.out.println("Dead methods");
-        }
-
-        public static void incPointer()
-        {
-                pointer++;
-        }
-
-        public static void add(long[] item)
-        {
-                Buffer[pointer] = item[0];
-                pointer++;
-                Buffer[pointer] = item[1];
-                pointer++;
-
-                if (pointer > 250_000_000) {
-                        rotateBuffer();   
-                }
-
-        }
-
-        public static void print(){
-                if (pointer == 0) {
-                        System.out.println("Buffer is empty! :(");
-                }
-                for (int i = 0; i < pointer; i++) {
-                        System.out.println(Buffer[i] + "," + Buffer[i+1]);
-                        i++;
-                }
-        }
 
 
 }
