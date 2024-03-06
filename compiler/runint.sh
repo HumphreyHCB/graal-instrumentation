a96b7958b31eb948252681efd090fe0d910227c7
Humphrey Burchell <humphreyburchell@gmail.com>
2024-03-06 17:34:51 +0000

    runner for rebench

---
+++ compiler/runint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=/home/hburchell/Repos/graal-dev/mx:$PATH
+exec mx --java-home /home/hburchell/Repos/graal-dev/labs-openjdk/build/linux-x86_64-server-release/images/jdk/ vm -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI \
+  -XX:+UseJVMCICompiler  \
+  --add-exports jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.Bubo=ALL-UNNAMED \
+  -cp /home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/mxbuild/dists/graal.jar:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler:benchmarks.jar \
+  -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
