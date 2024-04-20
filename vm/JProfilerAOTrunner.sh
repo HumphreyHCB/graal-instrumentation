de0202244b99f1c245f31b36b4f078b9f146d00d
Humphrey Burchell <humphreyburchell@gmail.com>
2024-04-20 22:38:40 +0100

    AOT runners

--- vm/JProfilerAOTrunner.sh
+++ vm/JProfilerAOTrunner.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
  ./latest_graalvm_home/bin/java -XX:+UseJVMCICompiler -XX:+UseJVMCINativeLibrary \
-  -Dgraal.EnableProfiler=true -Dgraal.MinGraphSize=80 -Dgraal.CountCompiledMethods=false -Dgraal.BuboDebugMode=false \
-  -Djdk.graal.PrintCompilation=true -Xlog:jit+compilation \
+   -Dgraal.EnableProfiler=false -Dgraal.MinGraphSize=80 -Dgraal.CountCompiledMethods=false -Dgraal.BuboDebugMode=false \
+  -Djdk.graal.LogFile=JProfilerAOT.txt -Djdk.graal.PrintCompilation=true  \
   --add-exports jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.Bubo=ALL-UNNAMED \
   -cp /home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/mxbuild/dists/graal.jar:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/benchmarks.jar \
-  -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar \
-  Harness DeltaBlue 30 60000  
+  -agentpath:/home/hburchell/jprofiler14/bin/linux-x64/libjprofilerti.so=port=8849,nowait,config=/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/jprofiler_config.xml \
+  Harness DeltaBlue 10 60000  
--- vm/YourkitAOTrunner.sh
+++ vm/YourkitAOTrunner.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
  ./latest_graalvm_home/bin/java -XX:+UseJVMCICompiler -XX:+UseJVMCINativeLibrary \
-  -Dgraal.EnableProfiler=true -Dgraal.MinGraphSize=80 -Dgraal.CountCompiledMethods=false -Dgraal.BuboDebugMode=false \
-  -Djdk.graal.PrintCompilation=true -Xlog:jit+compilation \
+   -Dgraal.EnableProfiler=false -Dgraal.MinGraphSize=80 -Dgraal.CountCompiledMethods=false -Dgraal.BuboDebugMode=false \
+  -Djdk.graal.LogFile=YourKitAOT.txt -Djdk.graal.PrintCompilation=true  \
   --add-exports jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.Bubo=ALL-UNNAMED \
   -cp /home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/mxbuild/dists/graal.jar:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/benchmarks.jar \
-  -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar \
-  Harness DeltaBlue 30 60000  
+  -agentpath:/home/hburchell/YourKit-JavaProfiler-2024.3/bin/linux-x86-64/libyjpagent.so=@profiler-options.txt \
+  Harness DeltaBlue 10 60000  
