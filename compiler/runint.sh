85508df74979bf132b0f7154a4cf23711b58be96
Humphrey Burchell <humphreyburchell@gmail.com>
2024-04-20 22:36:48 +0100

    Rebench changes

--- compiler/runint.sh
+++ compiler/runint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/home/hburchell/Repos/graal-dev/mx:$PATH
-exec mx --java-home /home/hburchell/Repos/graal-dev/labs-openjdk/build/linux-x86_64-server-release/images/jdk/ vm -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI \
-  -XX:+UseJVMCICompiler  \
+exec mx --java-home /home/hburchell/Downloads/labsjdk-ce-21.0.2-jvmci-23.1-b33 vm -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI \
+  -XX:+UseJVMCICompiler -Dgraal.EnableProfiler=true -Dgraal.MinGraphSize=80 \
   --add-exports jdk.graal.compiler/jdk.graal.compiler.hotspot.meta.Bubo=ALL-UNNAMED \
   -cp /home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/mxbuild/dists/graal.jar:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler:benchmarks.jar \
-  -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
+  -javaagent:/home/hburchell/Repos/graal-dev/graal-instrumentation/Bubo-Agent/target/JavaAgent-1.0-SNAPSHOT-jar-with-dependencies.jar"$@"
