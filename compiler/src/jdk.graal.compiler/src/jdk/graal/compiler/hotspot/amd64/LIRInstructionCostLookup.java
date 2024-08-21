5d251661a9f4690533631342d36b7a94167c48b2
Humphrey Burchell <humphreyburchell@gmail.com>
2024-08-21 19:18:24 +0100

    now reads json file

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/amd64/LIRInstructionCostLookup.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/amd64/LIRInstructionCostLookup.java
@@ -1,119 +1,62 @@
 package jdk.graal.compiler.hotspot.amd64;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
+import org.graalvm.collections.EconomicMap;
+import org.graalvm.collections.MapCursor;
+
+import jdk.graal.compiler.util.json.JsonParser;
 
 public class LIRInstructionCostLookup {
-    // Static map to hold the class names and their corresponding costs
-    private static final Map<String, Integer> CLASS_COST_MAP = new HashMap<>();
+    private static final EconomicMap<String, Integer> CLASS_COST_MAP = EconomicMap.create();
 
     static {
-        // Populate the map with class names and their costs
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.vector.AMD64VectorBlend$VexBlendOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$TestBranchOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$CmpConstBranchOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$TestConstBranchOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ConvertFloatToIntegerOp", 4);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.StandardOp$LabelOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$BitTestAndBranchOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotSafepointOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$BranchOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotIndirectCallOp", 3);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotStrategySwitchOp", 11);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$CondSetOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$HashTableSwitchOp", 9);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotCRuntimeCallPrologueOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$MemoryMROp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$AMD64StackMove", 4);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Binary$MemoryTwoOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64MathPowOp", 1022);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$MemoryConstOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$TestByteBranchOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$CondMoveOp", 5);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64SaveRegistersOp", 27);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ShiftOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64SignExtendOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotPatchReturnAddressOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotMove$HotSpotLoadMetaspaceConstantOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.vector.AMD64VectorBinary$AVXBinaryMemoryOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64MathCosOp", 412);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Binary$DataTwoOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64DeoptimizeOp", 3);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.vector.AMD64VectorBinary$AVXBinaryOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.vector.AMD64VectorBinary$AVXBinaryConstFloatOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$Op", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64MathLogOp", 138);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ZeroMemoryOp", 6);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$ConstOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotspotDirectVirtualCallOp", 4);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64MathSinOp", 415);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64PrefetchOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotUnwindOp", 4);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$RangeTableSwitchOp", 9);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.g1.AMD64G1PreWriteBarrierOp", 12);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Binary$CommutativeTwoOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotDeoptimizeCallerOp", 3);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$MemoryRMOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64MulDivOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64VectorizedMismatchOp", 113);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Unary$MemoryOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$CmpBranchOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$CompressPointerOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ByteSwapOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Call$DirectNearForeignCallOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$NullCheckOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Unary$MROp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotDirectStaticCallOp", 3);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Unary$MOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$MemoryVMConstOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Unary$RMOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotReturnOp", 6);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$MoveFromConstOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Binary$ConstOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64VectorizedHashCodeOp", 143);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64StringUTF16CompressOp", 68);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotMove$HotSpotLoadObjectConstantOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$CompareAndSwapOp", 5);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$StackLeaOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$AMD64MultiStackMove", 6);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$MembarOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$FloatBranchOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$LeaOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$FloatCondMoveOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Binary$RMIOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64RestoreRegistersOp", 27);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64BinaryConsumer$VMConstOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ArrayIndexOfOp", 128);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$AtomicReadAndAddOp", 4);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ControlFlow$FloatCondSetOp", 2);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64UnaryConsumer$MemoryOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$MoveToRegOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.StandardOp$JumpOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$MoveFromRegOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Move$UncompressPointerOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.vector.AMD64VectorFloatCompareOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64Binary$TwoOp", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64NormalizedUnsignedCompareOp", 5);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64ArrayEqualsOp", 17);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotCRuntimeCallEpilogueOp", 3);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.g1.AMD64G1PostWriteBarrierOp", 22);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64VZeroUpper", 1);
-        CLASS_COST_MAP.put("jdk.graal.compiler.hotspot.amd64.AMD64HotSpotJumpToExceptionHandlerInCallerOp", 4);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64SHA1Op", 245);
-        CLASS_COST_MAP.put("jdk.graal.compiler.lir.amd64.AMD64StringLatin1InflateOp", 46);
+        try {
+            loadClassCostsFromJSON("MandelbrotInstructionsAdjusted.json");
+        } catch (IOException e) {
+            System.err.println("Failed to load class costs: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Loads class costs from a JSON file into the CLASS_COST_MAP.
+     *
+     * @param filePath the path to the JSON file
+     * @throws IOException if there is an error reading the file
+     */
+    private static void loadClassCostsFromJSON(String filePath) throws IOException {
+        // Read the JSON file content
+        String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
+
+        // Parse the JSON content into an EconomicMap
+        JsonParser parser = new JsonParser(jsonContent);
+        EconomicMap<String, Object> jsonMap = (EconomicMap<String, Object>) parser.parse();
+
+        // Iterate over the entries using a MapCursor and populate CLASS_COST_MAP
+        MapCursor<String, Object> cursor = jsonMap.getEntries();
+        while (cursor.advance()) {
+            String key = cursor.getKey().replaceFirst("class ", "");
+            Integer value = (Integer) cursor.getValue();
+            CLASS_COST_MAP.put(key, value);
+        }
     }
 
     /**
      * Method to look up the cost of a given class.
      *
      * @param className the fully qualified name of the class
-     * @return the cost associated with the class, or -1 if the class is not found
+     * @return the cost associated with the class, or 1 if the class is not found
      */
     public static int getCost(String className) {
-        String sanitizedClassName = className.replaceFirst("class ", "");
-        //if (!CLASS_COST_MAP.containsKey(sanitizedClassName.trim())) {
-        //    System.out.println("Have no info for " + sanitizedClassName);
-        // }
-        return CLASS_COST_MAP.getOrDefault(sanitizedClassName.trim(), 1);
+        // Sanitize the class name and look it up in the map
+        String sanitizedClassName = className.replaceFirst("class ", "").trim();
+        if (CLASS_COST_MAP.containsKey(sanitizedClassName)) {
+            return CLASS_COST_MAP.get(sanitizedClassName);
+        } else {
+            //System.out.println("Missing " + className);
+            return 1; // Default value if the class is not found
+        }
     }
 }
