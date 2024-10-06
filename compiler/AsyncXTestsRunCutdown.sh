6070b8914f28d3a4f52956e75b6c1a24c3000698
Humphrey Burchell <humphreyburchell@gmail.com>
2024-10-06 15:37:25 +0100

    runners

--- compiler/AsyncXTestsRunCutdown.sh
+++ compiler/AsyncXTestsRunCutdown.sh
@@ -10,16 +10,16 @@
 #   -agentpath:/home/hburchell/ProgramFiles/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=cpu,interval=1ms,file=GTDump/AsyncNBodyNoSlowdownNOBack.txt \
 #   Harness NBody 1000 1200000  
 
-file=TowersAsyncSlowdownTest1.txt
+file=JsonAsyncNoSlowdownNoOSR.txt
 
 # Slowdown Iteration 1000
 mx  --java-home \
  /home/hburchell/Downloads/labsjdk-ce-21.0.2-jvmci-23.1-b33 \
- vm -Djdk.graal.EnableGTSlowDown=false -Djdk.graal.LIRGTSlowDown=true -XX:-TieredCompilation -XX:-BackgroundCompilation \
+ vm -Djdk.graal.EnableGTSlowDown=false -Djdk.graal.LIRGTSlowDown=false -XX:-TieredCompilation -XX:-BackgroundCompilation \
  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI  \
- -XX:+UseJVMCICompiler -Djdk.graal.TrackNodeSourcePosition=true -XX:-UseOnStackReplacement '-XX:CompileCommand=dontinline,*::*' -Djdk.graal.TrivialInliningSize=0 \
+ -XX:+UseJVMCICompiler -Djdk.graal.TrackNodeSourcePosition=true '-XX:CompileCommand=dontinline,*::*' -Djdk.graal.TrivialInliningSize=0 \
  -cp /home/hburchell/Repos/graal-dev/graal-instrumentation/compiler/mxbuild/dists/graal.jar:/home/hburchell/Repos/graal-dev/graal-instrumentation/compiler:benchmarks.jar \
  -agentpath:/home/hburchell/ProgramFiles/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=cpu,interval=1ms,file=$file \
- Harness Towers 200 2500 
+ Harness Json 200 100 
 
 echo "Wrote to here $file"
