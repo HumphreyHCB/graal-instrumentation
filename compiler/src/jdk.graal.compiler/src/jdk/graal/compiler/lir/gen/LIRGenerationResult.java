39514e55a538675fc9358b924492d35ba3f7c5ab
Humphrey Burchell <humphreyburchell@gmail.com>
2024-12-05 11:24:02 +0000

    getCompilationUnitName overload

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/gen/LIRGenerationResult.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/gen/LIRGenerationResult.java
@@ -159,6 +159,13 @@ public class LIRGenerationResult {
         return compilationId.toString(Verbosity.NAME);
     }
 
+    public String getCompilationUnitName(Verbosity verbosity) {
+        if (compilationId == null || compilationId == CompilationIdentifier.INVALID_COMPILATION_ID) {
+            return "<unknown>";
+        }
+        return compilationId.toString(verbosity);
+    }
+
     /**
      * Return the first position to insert a LIR instruction. No instruction should be inserted
      * before this position.
