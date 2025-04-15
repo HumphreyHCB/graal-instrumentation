0f75bf40bfaae2c6eac5239b90a913dc2e9189de
Humphrey Burchell <humphreyburchell@gmail.com>
2025-04-15 14:12:20 +0100

    no Longer Need initalizeGT

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotGraalRuntime.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotGraalRuntime.java
@@ -82,6 +82,7 @@ import jdk.vm.ci.hotspot.HotSpotJVMCIRuntime;
 import jdk.vm.ci.hotspot.HotSpotVMConfigStore;
 import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.MetaAccessProvider;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
 import jdk.vm.ci.runtime.JVMCIBackend;
 
 //JaCoCo Exclude
@@ -126,6 +127,8 @@ public final class HotSpotGraalRuntime implements HotSpotGraalRuntimeProvider {
     private final Map<ExceptionAction, Integer> compilationProblemsPerAction;
 
     private final CompilerProfiler compilerProfiler;
+
+    public static ResolvedJavaMethod GRAAL_DUMMY_METHOD = null;
 
     /**
      * @param nameQualifier a qualifier to be added to this runtime's {@linkplain #getName() name}
@@ -199,7 +202,14 @@ public final class HotSpotGraalRuntime implements HotSpotGraalRuntimeProvider {
         runtimeStartTime = System.nanoTime();
         bootstrapJVMCI = config.getFlag("BootstrapJVMCI", Boolean.class);
         if (GraalOptions.EnableGTSlowDown.getValue(options) || CompilationResultBuilder.Options.CollectLIRCostInformation.getValue(options)) {
-            initalizeGT();
+            //initalizeGT();
+            // try {
+            //     GRAAL_DUMMY_METHOD = getHostProviders().getMetaAccess().lookupJavaMethod(GTCache.class.getDeclaredMethod("TestPrint", new Class<?>[0]));
+            // } catch (Exception e) {
+            //     e.printStackTrace();
+            // }
+            // System.out.println("WE DID SOMTHING");           
+            
         }
 
         if(GraalOptions.LIRBlockSlowdownFileName.getValue(options) != GraalOptions.LIRCostFileName.getDefaultValue() ){
@@ -232,28 +242,28 @@ public final class HotSpotGraalRuntime implements HotSpotGraalRuntimeProvider {
 
     private void initalizeGT() {
         System.out.println("Groundtruth SlowDown Started......");
-        
-        GTCache timeCache = new GTCache(options);
-        timeCache.start();
+        //context.getMetaAccess().lookupJavaMethod(GTCache.class.getDeclaredMethod("TestPrint", new Class<?>[0]))
+        //GTCache timeCache = new GTCache(options);
+        //timeCache.start();
 
 
-        Thread writingHook = new Thread(() -> {
-            try {
-                timeCache.join();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            if (CompilationResultBuilder.Options.CollectLIRCostInformation.getValue(options)) {
-                GTCache.postProcessingShutdown();
-            }
-            if (GraalOptions.LIRGTSlowDown.getValue(options)) {
-                // do we need to anything??
-            }
+        // Thread writingHook = new Thread(() -> {
+        //     try {
+        //         timeCache.join();
+        //     } catch (InterruptedException e) {
+        //         // TODO Auto-generated catch block
+        //         e.printStackTrace();
+        //     }
+        //     if (CompilationResultBuilder.Options.CollectLIRCostInformation.getValue(options)) {
+        //         GTCache.postProcessingShutdown();
+        //     }
+        //     if (GraalOptions.LIRGTSlowDown.getValue(options)) {
+        //         // do we need to anything??
+        //     }
             
-            System.out.println("Groundtruth SlowDown Shutdown......");
-    });
-        Runtime.getRuntime().addShutdownHook(writingHook);
+        //     System.out.println("Groundtruth SlowDown Shutdown......");
+    //);
+       // Runtime.getRuntime().addShutdownHook(writingHook);
     }
 
     /**
